22de602874097a0306c7521fa02b719a7919fe19
Igor Ribeiro <igorribeiro4467@gmail.com>
2023-01-22 16:56:00 -0300

    Set configs for BillingProvider model.

--- app/Models/BillingProvider.php
+++ app/Models/BillingProvider.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class BillingProvider extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'description',
+        'tag',
+    ];
+
+    protected $casts = [
+        'params' => 'array'
+    ];
 }
