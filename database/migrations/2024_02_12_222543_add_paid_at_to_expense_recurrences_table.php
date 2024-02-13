f396a5473116018bd3dc027840fe641e74a22b68
Igor Ribeiro <igorribeiro4467@gmail.com>
2024-02-12 22:28:26 -0300

    Add column paid_at to expenses_recurrences table.

---
+++ database/migrations/2024_02_12_222543_add_paid_at_to_expense_recurrences_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('expense_recurrences', function (Blueprint $table) {
+            $table->dateTime('paid_at')->nullable()->after('paid');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('expense_recurrences', function (Blueprint $table) {
+            $table->dropColumn('paid_at');
+        });
+    }
+};
